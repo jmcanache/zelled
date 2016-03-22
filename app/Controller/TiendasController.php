00f5e491baf0825b0df7c0570d5493ca9d8abb44
jose <canache39@gmail.com>
2016-03-22 12:16:48 -0430

    reintentar nueva direccion

--- app/Controller/TiendasController.php
+++ app/Controller/TiendasController.php
@@ -72,7 +72,7 @@ class TiendasController extends AppController  {
 						//El usuario no debe volver atras en el navegador.
 						//Esta carpeta se debe crear una unica vez.		
 						
-						$thepath = 'app-root' . DS . 'data'. DS . 'todosproductos' . DS . $prefix;
+						$thepath = '~' . DS .'app-root' . DS . 'data'. DS . 'todosproductos' . DS . $prefix;
 						//$thepath = WWW_ROOT."todosproductos". DS . $prefix; // todosproductos es una carpeta que ya esta creada. alli van todas las carpetas de las tiendas
 						$crearCarpeta = $this->imagenesUtilidades->crearCarpeta($thepath);
 						
